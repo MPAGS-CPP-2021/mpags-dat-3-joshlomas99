3128c3827c533d21e303c0ebc7e0fd89141f9ec7
joshlomas99 <63054535+joshlomas99@users.noreply.github.com>
2021-10-31 16:46:43 +0000

    Implement tests for CaesarCipher class.

--- src/Testing/testCaesarCipher.cpp
+++ src/Testing/testCaesarCipher.cpp
@@ -4,7 +4,80 @@
 
 #include "CaesarCipher.hpp"
 
-// TEST_CASE("", "[]") {
+TEST_CASE("Initialize with integer key value", "[integer key]") {
 
-//     SECTION("") {
-//         REQUIRE();
+    CaesarCipher caesarCipher{12};
+
+    SECTION("Correctly encrypts input text") {
+        std::string inputText{"ABCDEFGHIJKLMN"};
+        std::string outputText{caesarCipher.applyCipher(inputText, CipherMode::Encrypt)};
+        REQUIRE(outputText == "MNOPQRSTUVWXYZ");
+    }
+
+    SECTION("Correctly wraps around alphabet on encyrption") {
+        std::string inputText{"MNOPQRSTUVWXYZ"};
+        std::string outputText{caesarCipher.applyCipher(inputText, CipherMode::Encrypt)};
+        REQUIRE(outputText == "YZABCDEFGHIJKL");
+    }
+
+    SECTION("Correctly decrypts input text") {
+        std::string inputText{"MNOPQRSTUVWXYZ"};
+        std::string outputText{caesarCipher.applyCipher(inputText, CipherMode::Decrypt)};
+        REQUIRE(outputText == "ABCDEFGHIJKLMN");
+    }
+
+    SECTION("Correctly wraps around alphabet on decryption") {
+        std::string inputText{"ABCDEFGHIJKLMN"};
+        std::string outputText{caesarCipher.applyCipher(inputText, CipherMode::Decrypt)};
+        REQUIRE(outputText == "OPQRSTUVWXYZAB");
+    }
+
+}
+
+TEST_CASE("Initialize with valid string key value", "[string key]") {
+
+    CaesarCipher caesarCipher{"12"};
+
+    SECTION("Correctly encrypts input text") {
+        std::string inputText{"ABCDEFGHIJKLMN"};
+        std::string outputText{caesarCipher.applyCipher(inputText, CipherMode::Encrypt)};
+        REQUIRE(outputText == "MNOPQRSTUVWXYZ");
+    }
+
+    SECTION("Correctly wraps around alphabet on encyrption") {
+        std::string inputText{"MNOPQRSTUVWXYZ"};
+        std::string outputText{caesarCipher.applyCipher(inputText, CipherMode::Encrypt)};
+        REQUIRE(outputText == "YZABCDEFGHIJKL");
+    }
+
+    SECTION("Correctly decrypts input text") {
+        std::string inputText{"MNOPQRSTUVWXYZ"};
+        std::string outputText{caesarCipher.applyCipher(inputText, CipherMode::Decrypt)};
+        REQUIRE(outputText == "ABCDEFGHIJKLMN");
+    }
+
+    SECTION("Correctly wraps around alphabet on decryption") {
+        std::string inputText{"ABCDEFGHIJKLMN"};
+        std::string outputText{caesarCipher.applyCipher(inputText, CipherMode::Decrypt)};
+        REQUIRE(outputText == "OPQRSTUVWXYZAB");
+    }
+
+}
+
+TEST_CASE("Initialize with invalid string key value", "[string key]") {
+
+    CaesarCipher caesarCipher{"12a"};
+
+    SECTION("Correctly encrypts input text with key at default value of 0") {
+        std::string inputText{"ABCDEFGHIJKLMN"};
+        std::string outputText{caesarCipher.applyCipher(inputText, CipherMode::Encrypt)};
+        REQUIRE(outputText == "ABCDEFGHIJKLMN");
+    }
+
+    SECTION("Correctly decrypts input text with key at default value of 0") {
+        std::string inputText{"ABCDEFGHIJKLMN"};
+        std::string outputText{caesarCipher.applyCipher(inputText, CipherMode::Decrypt)};
+        REQUIRE(outputText == "ABCDEFGHIJKLMN");
+    }
+
+}
